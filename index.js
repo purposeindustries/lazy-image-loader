d5191e3149b16a9dc55d49049610b1e51c869d7b
ruff <ftamas.mail@gmail.com>
2015-07-07 14:51:57 +0200

    readable custom url handling

--- index.js
+++ index.js
@@ -26,7 +26,11 @@
 
   return function init(host, options) {
     options = options || {};
-    var url = (typeof options.url === 'function') ? options.url : function () {};
+    var url = (typeof options.url === 'function') ?
+      options.url :
+      function (width, path) {
+        return host + '/' + width + path;
+      };
     var items = [].slice.call(document.querySelectorAll('.lazy-image'));
     var len = items.length;
     var sample;
@@ -36,7 +40,7 @@
 
       var path = item.getAttribute('data-path');
       var img = document.createElement('img');
-      img.src = (url() || host + '/' + Math.round(sample.width)) + path;
+      img.src = url(Math.round(sample.width), path);
 
       item.appendChild(img);
 
